61d2f71931c7b770dabd3e7343c8c1fc52107271
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2024-09-29 18:37:37 +0800

    Update app_funktionalitaet.py
    Gemini-Optimierung

--- app_funktionalitaet.py
+++ app_funktionalitaet.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Diese Datei enthält die Beschreibung (Klasse) der graphischen Benutzeroberfläche der Stick-App.
+This file contains the description (class) of the graphical user interface of the Stick-App.
 
 Created on Sat Apr 27 18:34:16 2024
 
@@ -11,14 +11,12 @@ import tkinter as tk
 import tkinter.filedialog as fd
 import tkinter.ttk as ttk
 import tkinter.messagebox as mb
-
 from os import path
-
 from PIL import ImageTk, Image
 
 import stick_funktionalitaet as sf
 
-class stick_app(tk.Frame):
+class StickApp(tk.Frame):
     """
     A GUI application for generating embroidery patterns from images.
 
@@ -27,49 +25,54 @@ class stick_app(tk.Frame):
     - "Stickmuster erzeugen" (Generate Embroidery Pattern): Processes the selected
       image and generates an embroidery pattern, saving it to a file chosen by the user.
     """
-    
-    def __init__(self,master):
+
+    def __init__(self, master):
         """
-        Initializes the stick_app GUI.
+        Initializes the StickApp GUI.
 
         Args:
             master: The parent Tkinter widget.
         """
         super().__init__(master)
-        self.eingabe_datei = None
+        self.input_file = None
         self.pack()
-        self.ladeknopf = ttk.Button(self, text="Bild auswählen", command=self.laden)
-        self.ladeknopf.pack()
-        self.startknopf = ttk.Button(self, text="Stickmuster erzeugen", command=self.starten)
-        self.startknopf.pack()
-        self.bild = ttk.Label(self)
-        self.bild.pack()
-        
-        
-    def laden(self):
+
+        # Buttons
+        self.load_button = ttk.Button(self, text="Bild auswählen", command=self.load_image)
+        self.load_button.pack()
+        self.start_button = ttk.Button(self, text="Stickmuster erzeugen", command=self.generate_pattern)
+        self.start_button.pack()
+
+        # Image display label
+        self.image_label = ttk.Label(self)
+        self.image_label.pack()
+
+    def load_image(self):
         """
         Opens a file dialog to select an image and displays it in the GUI.
         """
-        self.eingabe_datei = fd.askopenfilename()
-        zwischen_bild = Image.open(self.eingabe_datei)
-        zwischen_bild = zwischen_bild.resize((zwischen_bild.width*700//zwischen_bild.height,700))
-        eingabe_bild = ImageTk.PhotoImage(zwischen_bild)
-        self.bild.configure(image=eingabe_bild)
-        self.bild.image = eingabe_bild
-        
-    def starten(self):
+        self.input_file = fd.askopenfilename()
+        if self.input_file:
+            image = Image.open(self.input_file)
+            image = image.resize((image.width * 700 // image.height, 700))
+            photo = ImageTk.PhotoImage(image)
+            self.image_label.configure(image=photo)
+            self.image_label.image = photo
+
+    def generate_pattern(self):
         """
         Generates an embroidery pattern from the selected image and displays it.
         """
-        if self.eingabe_datei == None:
+        if self.input_file is None:
             mb.showerror(message="Keine Datei ausgewählt")
         else:
-            ausgabe_string = path.split(path.splitext(self.eingabe_datei)[0])
-            ausgabe_string = 'Stickmuster_' + ausgabe_string[1] + '.jpg'
-            self.ausgabe_datei = fd.asksaveasfile(initialfile=ausgabe_string,defaultextension='jpg')
-            sf.muster_generieren(self.eingabe_datei, self.ausgabe_datei)  
-            zwischen_bild = Image.open(self.ausgabe_datei.name)
-            zwischen_bild = zwischen_bild.resize((zwischen_bild.width*700//zwischen_bild.height,700))
-            ausgabe_bild = ImageTk.PhotoImage(zwischen_bild)
-            self.bild.configure(image=ausgabe_bild)
-            self.bild.image = ausgabe_bild
+            output_filename = path.split(path.splitext(self.input_file)[0])
+            output_filename = 'Stickmuster_' + output_filename[1] + '.jpg'
+            output_file = fd.asksaveasfile(initialfile=output_filename, defaultextension='jpg')
+            if output_file:
+                sf.muster_generieren(self.input_file, output_file)
+                image = Image.open(output_file.name)
+                image = image.resize((image.width * 700 // image.height, 700))
+                photo = ImageTk.PhotoImage(image)
+                self.image_label.configure(image=photo)
+                self.image_label.image = photo
