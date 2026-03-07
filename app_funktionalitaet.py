9dfed4a618863b347e7a36691c332aa2ac1c73f9
ThorstenClausing <163404932+ThorstenClausing@users.noreply.github.com>
2026-03-07 17:12:53 +0100

    Refactor image loading and pattern generation methods

--- app_funktionalitaet.py
+++ app_funktionalitaet.py
@@ -25,16 +25,20 @@ class StickApp(tk.Frame):
     - "Stickmuster erzeugen" (Generate Embroidery Pattern): Processes the selected
       image and generates an embroidery pattern, saving it to a file chosen by the user.
     """
-
-    def __init__(self, master):
+    CONFIG = {'input_file':None,
+              'kmeans_n_clusters':20,
+              'crosses_x':150}
+    
+    def __init__(self, master, config=CONFIG):
         """
         Initializes the StickApp GUI.
 
         Args:
             master: The parent Tkinter widget.
+            config: A dictionary with configuration data
         """
         super().__init__(master)
-        self.input_file = None
+        self.config=config
         self.pack()
 
         # Buttons
@@ -42,6 +46,8 @@ class StickApp(tk.Frame):
         self.load_button.pack()
         self.start_button = ttk.Button(self, text="Stickmuster erzeugen", command=self.generate_pattern)
         self.start_button.pack()
+        self.start_ohne_button = ttk.Button(self, text="Stickmuster ohne Hintergrund erzeugen", command=self.generate_pattern_without_bg)
+        self.start_ohne_button.pack()
 
         # Image display label
         self.image_label = ttk.Label(self)
@@ -51,9 +57,9 @@ class StickApp(tk.Frame):
         """
         Opens a file dialog to select an image and displays it in the GUI.
         """
-        self.input_file = fd.askopenfilename()
-        if self.input_file:
-            image = Image.open(self.input_file)
+        self.config['input_file'] = fd.askopenfilename()
+        if self.config['input_file']:
+            image = Image.open(self.config['input_file'])
             image = image.resize((image.width * 700 // image.height, 700))
             photo = ImageTk.PhotoImage(image)
             self.image_label.configure(image=photo)
@@ -63,14 +69,41 @@ class StickApp(tk.Frame):
         """
         Generates an embroidery pattern from the selected image and displays it.
         """
-        if self.input_file is None:
+        if self.config['input_file'] is None:
             mb.showerror(message="Keine Datei ausgewählt")
         else:
-            output_filename = path.split(path.splitext(self.input_file)[0])
+            output_filename = path.split(path.splitext(self.config['input_file'])[0])
             output_filename = 'Stickmuster_' + output_filename[1] + '.jpg'
             output_file = fd.asksaveasfile(initialfile=output_filename, defaultextension='jpg')
             if output_file:
-                sf.muster_generieren(self.input_file, output_file)
+                sf.muster_generieren(self.config['input_file'], 
+                                     output_file,
+                                     remove_background=False,
+                                     kmeans_n_clusters=self.config['kmeans_n_clusters'],
+                                     crosses_x=self.config['crosses_x'])
+                image = Image.open(output_file.name)
+                image = image.resize((image.width * 700 // image.height, 700))
+                photo = ImageTk.PhotoImage(image)
+                self.image_label.configure(image=photo)
+                self.image_label.image = photo
+                
+    def generate_pattern_without_bg(self):
+        """
+        Generates an embroidery pattern from the selected image, trying to remove 
+        the backgroud, and displays it.
+        """
+        if self.config['input_file'] is None:
+            mb.showerror(message="Keine Datei ausgewählt")
+        else:
+            output_filename = path.split(path.splitext(self.config['input_file'])[0])
+            output_filename = 'Stickmuster_' + output_filename[1] + '.jpg'
+            output_file = fd.asksaveasfile(initialfile=output_filename, defaultextension='jpg')
+            if output_file:
+                sf.muster_generieren(self.config['input_file'], 
+                                     output_file,
+                                     remove_background=True, 
+                                     kmeans_n_clusters=self.config['kmeans_n_clusters'],
+                                     crosses_x=self.config['crosses_x'])
                 image = Image.open(output_file.name)
                 image = image.resize((image.width * 700 // image.height, 700))
                 photo = ImageTk.PhotoImage(image)
